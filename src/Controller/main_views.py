65ca2248e74864493bb68e5f9f3b3a812cf4f940
박준영 <jun_1204@naver.com>
2022-10-10 17:25:50 +0900

    구조 수정

--- src/Controller/main_views.py
+++ src/Controller/main_views.py
@@ -1,9 +1,29 @@
 import pymongo as pymongo
-from flask import Blueprint
+from flask import Blueprint, request, render_template
 
 main_page = Blueprint("main", __name__, url_prefix='/')
 
-
+# 메인 화면
 @main_page.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html')
+
+# 상품 목록
+@main_page.route('/goods', methods = ['GET'])
+def get_goods_page():
+
+    # 상품 정보를 담은 json 전달
+    data = {"name" : "새우깡"}
+
+    return data
+
+# 결과 지도 화면
+@main_page.route('/result', methods = ['POST'])
+def get_result_page():
+    # 상품 목록과 현재 위치를 담은 json 파일 받을 예정
+    if request.is_json:
+        data = request.get_json()
+    else:
+        return None
+
+    return data
