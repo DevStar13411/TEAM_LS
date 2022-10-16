ee0fe067dd1c6974bb9783223785cce147a41a3d
박준영 <jun_1204@naver.com>
2022-10-16 18:01:34 +0900

    상품 가격 조회 기능 추가

--- src/Model/control_db.py
+++ src/Model/control_db.py
@@ -1,3 +1,6 @@
+import time
+
+from flask import json
 from pymongo import MongoClient
 
 from config import db_client
@@ -11,6 +14,7 @@ db = client.zzangbaguni
 # collect_entp = db.entplist
 # collect_price = db.price
 
+# 모든 상품 코드 불러오기
 def find_all_good_by_id():
     collection_good = db.goodlist
     all_row = collection_good.find()
@@ -21,15 +25,17 @@ def find_all_good_by_id():
 
 
 # data type(collection) 에 따라 control 가능하도록 구현
+# 날짜 최신으로 갱신하는 형태로 작성해야함
 def insert_row(coll, add_row):
     collect_price = db.price
     collect_price.insert_many(add_row)
 
 
 # update 업체의 위도 경도
-def update_entp_pos(id,lat,long):
+def update_entp_pos(id, lat, long):
     collect_entp = db.entplist
-    collect_entp.update_one({"entpId":id}, {"$set":{"latitude": lat, "longitude": long}}, upsert=True)
+    collect_entp.update_one({"entpId": id}, {"$set": {"latitude": lat, "longitude": long}}, upsert=True)
+
 
 # 모든 업체 불러오기
 def find_all_entp():
@@ -37,3 +43,61 @@ def find_all_entp():
     all_entp = collect_entp.find()
     return all_entp
 
+
+# 상품 이름, 카테고리로 정리(상품 코드 AAAAAXXX : 앞 다섯자리)
+# 30101 30102 30103 30201 30202 30203 30204 30205 30206 30301 30302 30304 30305
+# 후에 카테고리 명칭 정해지면 분류해서 출력
+def find_all_good_category():
+    collection_good = db.goodlist
+    all_row = collection_good.find()
+    info = []
+    for row in all_row:
+        category_number = row['goodSmlclsCode'] // 1000
+        info.append({"name": row["goodName"], "category": category_number})
+
+    return info
+
+
+# 각 업체 별 상품 개수 체크
+def find_entp(entp_id_list):
+
+    collect_entp = db.entplist
+    collect_price = db.price
+
+    for entp in entp_id_list:
+
+        entp_name = collect_entp.find_one({"entpId": entp})["entpName"]
+        row = collect_price.find({"entpId": entp})
+        row = list(row)
+        print(entp_name, len(row),347)
+
+
+# 상품 id, 주변 업체 id list -> 상품 명, 업체 명, 가격 정보 출력
+# 최적화 필요함 ... 필수...
+def find_price_by_id(good_id_list, entp_id_list):
+
+    collection_good = db.goodlist
+    collect_entp = db.entplist
+    collect_price = db.price
+    info =[]
+    data_entp = []
+
+    for entp in entp_id_list:
+        cnt = 0
+        total = 0
+        for good_id in good_id_list:
+            total +=1
+            row = collect_price.find_one({"goodId": good_id, "entpId": entp})
+            good_name = collection_good.find_one({"goodId": good_id})["goodName"]
+            entp_name = collect_entp.find_one({"entpId": entp})["entpName"]
+            try:
+                info.append({"good_name":good_name ,"entp_name":entp_name,"price": row["goodPrice"]})
+
+            except TypeError:
+                cnt += 1
+                # print("해당 업체("+entp_name+")는 상품("+good_name+")이 존재하지 않습니다.", )
+                pass
+        print("해당 업체("+entp_name+")는" ,cnt*100/total,"% 존재하지 않습니다.")
+        data_entp.append({"name": entp_name, "fail": cnt})
+
+    return info, data_entp
