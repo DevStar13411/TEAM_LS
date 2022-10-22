bb51e83f0372bf70c92b946988b79b60ea9364bd
CodeButter96 <90555140+CodeButter96@users.noreply.github.com>
2022-10-23 04:01:14 +0900

    router 설치

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import axios from 'axios';
 
-createApp(App).mount('#app')
+const app = createApp(App).use(router);
+app.config.globalProperties.axios = axios;
+app.mount('#app');
