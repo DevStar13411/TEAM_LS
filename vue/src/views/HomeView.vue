2190d301c48c22fa2e0445900f45b719b807b8f5
CodeButter96 <90555140+CodeButter96@users.noreply.github.com>
2022-10-23 04:56:47 +0900

    router설치, HomeView에 홈페이지 작성

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="logo" src="../assets/logo_small.png">
+    <HelloWorld msg="Welcome to ZzangBaguni"/>
   </div>
 </template>
 
