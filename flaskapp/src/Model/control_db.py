96dd99e9e8a2671288a2f0fcbf621ab637aa472c
박준영 <jun_1204@naver.com>
2022-11-08 17:56:58 +0900

    가격 정보 호출 API 개선

--- flaskapp/src/Model/control_db.py
+++ flaskapp/src/Model/control_db.py
@@ -1,6 +1,4 @@
-import time
 
-from flask import json
 from pymongo import MongoClient
 from config import db_client
 from src.Model.openapi import load_price
@@ -9,16 +7,13 @@ client = MongoClient(db_client)
 db = client.zzangbaguni
 
 
-# collection_good = db.goodlist
-# collect_entp = db.entplist
-# collect_price = db.price
-
+# 모든 상품 불러오기
 def find_all_good():
     collection_good = db.goodlist
     return list(collection_good.find({},{"goodId":1,"goodName":1,"goodSmlclsCode":1,"_id":0}))
 
 
-# 모든 상품 코드 불러오기
+# 모든 상품 ID 불러오기
 def find_all_good_by_id():
     collection_good = db.goodlist
     all_row = collection_good.find()
@@ -28,24 +23,17 @@ def find_all_good_by_id():
     return goodId
 
 
-# data type(collection) 에 따라 control 가능하도록 구현
-# 날짜 최신으로 갱신하는 형태로 작성해야함
-def insert_row(coll, add_row):
-    collect_price = db.price
-    collect_price.insert_many(add_row)
-
-
 # update 업체의 위도 경도
 def update_entp_pos(id, lat, long):
     collect_entp = db.entplist
     collect_entp.update_one({"entpId": id}, {"$set": {"latitude": lat, "longitude": long}}, upsert=True)
 
+# update 가격 정보 2주 단위로 실행할 수 있도록 작성
 def update_price(date= "20221021"):
     data = load_price(date)
     collect_price = db.price
     cnt = 0
-    # with open("price"+date+".json", "r") as file:
-    #     data = json.load(file)
+
     for row in data:
         collect_price.replace_one({"goodId":row['goodId'],"entpId":row['entpId']},row,upsert=True)
 
@@ -62,7 +50,6 @@ def find_all_entp():
 
 # 상품 이름, 카테고리로 정리(상품 코드 AAAAAXXX : 앞 다섯자리)
 # 30101 30102 30103 30201 30202 30203 30204 30205 30206 30301 30302 30304 30305
-# 후에 카테고리 명칭 정해지면 분류해서 출력
 def find_all_good_category():
     collection_good = db.goodlist
     all_row = collection_good.find()
@@ -70,26 +57,27 @@ def find_all_good_category():
     for row in all_row:
         category_number = row['goodSmlclsCode'] // 1000
         info.append({"name": row["goodName"], "category": category_number})
-
     return info
 
 
-# 각 업체 별 상품 개수 체크
+# id_list 안에 있는 매장 정보 출력
 def find_entp(entp_id_list):
 
     collect_entp = db.entplist
-    collect_price = db.price
-
-    for entp in entp_id_list:
-
-        entp_name = collect_entp.find({"entpId": entp})["entpName"]
-        row = collect_price.find({"entpId": entp})
-        row = list(row)
-        print(entp_name, len(row),347)
+    result = list(collect_entp.find({"entpId": {"$in": entp_id_list}},
+                                       {"entpId": 1, "entpName": 1, "latitude": 1, "longitude": 1, "_id": 0}))
+    return result
 
 
-# 상품 id, 주변 업체 id list -> 상품 명, 업체 명, 가격 정보 출력
-#
+# id_list 안에 있는 상품 정보 출력
+def find_good(good_id_list):
+    collection_good = db.goodlist
+    result = list(collection_good.find({"goodId": {"$in": good_id_list}}, {"goodId": 1, "goodName": 1, "_id": 0}))
+
+    return result
+
+
+# 상품 id, 주변 업체 id list -> 상품 명, 업체 명, 가격 정보 출력(개선 전)
 def find_price_by_id(good_id_list, entp_id_list):
 
     collection_good = db.goodlist
@@ -98,28 +86,15 @@ def find_price_by_id(good_id_list, entp_id_list):
     info =[]
     data_entp = []
 
-    price_row = list(collect_price.find({"entpId":{"$in":entp_id_list},"goodId":{"$in":good_id_list}},{"entpId":1,"goodId":1,"goodPrice":1,"_id":0}))
+    price_row = list(collect_price.find({"entpId":{"$in":entp_id_list},"goodId":{"$in":good_id_list}},\
+                                        {"entpId":1,"goodId":1,"goodPrice":1,"_id":0}))
     entp_name = list(collect_entp.find({"entpId": {"$in" :entp_id_list}},{"entpId":1,"entpName":1,"latitude":1,"longitude":1,"_id":0}))
     good_name = list(collection_good.find({"goodId": {"$in" :good_id_list}},{"goodId":1,"goodName":1,"_id":0}))
-
-
-    # print(price_row)
-    # print(entp_name)
-    # print(good_name)
-    # for row in price_row:
-    #     good_name = collection_good.find_one({"goodId": row['goodId']})["goodName"]
-    #     entp_name = collect_entp.find_one({"entpId": row['entpId']})["entpName"]
-    #     try:
-    #         info.append({"good_name":good_name,"entp_name":entp_name,"price": row["goodPrice"]})
-    #
-    #     except TypeError:
-    #
-    #         # print("해당 업체("+entp_name+")는 상품("+good_name+")이 존재하지 않습니다.", )
-    #         pass
 
     return price_row, entp_name, good_name
 
 
+# 업체가 보유하고 있는 상품 리스트
 def find_good_by_entp(entp_id_list):
     collection_good = db.goodlist
     collect_price = db.price
@@ -133,3 +108,45 @@ def find_good_by_entp(entp_id_list):
     good_id_list = list(good_id_list)
 
     return list(collection_good.find({"goodId":{"$in":good_id_list}}, {"goodId": 1, "goodName": 1, "goodSmlclsCode": 1, "_id": 0}))
+
+
+# 개선된 업체 정보
+# ex) {123:"롯데마트"}
+def test_entp(entp_id_list):
+    collect_entp = db.entplist
+    entp_name = list(collect_entp.find({"entpId": {"$in" :entp_id_list}},{"entpId":1,"entpName":1,"_id":0}))
+    result = {}
+    for row in entp_name:
+        result[row['entpId']] = row['entpName']
+
+    return result
+
+
+# 개선된 상품 정보
+# ex) {991:"새우깡"}
+def test_good(good_id_list):
+    collection_good = db.goodlist
+    good_name = list(collection_good.find({"goodId": {"$in" :good_id_list}},{"goodId":1,"goodName":1,"_id":0}))
+    result = {}
+    for row in good_name:
+        result[row['goodId']] = row['goodName']
+
+    return result
+
+# 상품 id, 주변 업체 id list -> 상품 명, 업체 명, 가격 정보 출력(개선 후)
+def find_price(good_id_list, entp_id_list):
+
+    collect_price = db.price
+
+    result = {}
+    good = test_good(good_id_list)
+    entp = test_entp(entp_id_list)
+
+    for eid in entp_id_list:
+        price_row = list(collect_price.find({"entpId":eid,"goodId":{"$in":good_id_list}},\
+                                        {"goodId":1,"goodPrice":1,"_id":0}))
+        price_coll = {}
+        for row in price_row:
+            price_coll[good[row['goodId']]]= row['goodPrice']
+        result[entp[eid]] = price_coll
+    return result
--- flaskapp/test/test_openapi.py
+++ flaskapp/test/test_openapi.py
@@ -1,38 +1,49 @@
-
 import time
 from unittest import TestCase
 
-from flaskapp.src.Model.control_db import find_all_good_by_id, find_entp, find_price_by_id, find_all_good_category, \
-    update_price
-from flaskapp.src.Model.openapi import load_good, trans_jsonfile
-from flaskapp.src.Service.address import update_entp_position, find_near_entp
+
+from src.Model.control_db import find_all_good_by_id, find_price_by_id, \
+    update_price, find_price, find_good, find_entp
+
+from src.Service.address import find_near_entp
 
 
 class Test(TestCase):
-    def test_update_pos(self):
+    def test_main(self):
+
+        self.test_price(1)
+        self.test_price(2)
+
+    def test_price(self, num):
+
         start = time.time()
-        update_entp_position()
+        # 테스트 환경 : 모든 상품목록, 학교 주변 매장
+        if num == 1:
+            self.test_price1()
+        elif num == 2:
+            self.test_price2()
+
         end = time.time()
-        print(f"{end - start:.5f} sec")
+        print("test", num, f"{end - start:.5f} sec")
 
-    def test_price_info(self):
-        start = time.time()
-        good_id_list =find_all_good_by_id()
+    def test_price1(self):
 
-        # 지관 좌표
-        # near_entp = find_near_entp((37.29640641606932,126.9776123527779),5)
-        near_entp = find_near_entp(( 37.265903839248956, 126.9999098091105), 5)
+        good_id_list = find_all_good_by_id()
+        entp_id_list = find_near_entp((37.265903839248956, 126.9999098091105), 5)
 
-        # 필요한 상품 list에 따른 주변 매장 상품 가격 정보
-        find_price_by_id(good_id_list,near_entp)
-        end = time.time()
-        print(f"{end - start:.5f} sec")
+        price_row, entp_name, good_name = find_price_by_id(good_id_list, entp_id_list)
 
+    def test_price2(self):
+
+        good_id_list = find_all_good_by_id()
+        entp_id_list = find_near_entp((37.265903839248956, 126.9999098091105), 5)
+
+        good = find_good(good_id_list)
+        entp = find_entp(entp_id_list)
+        price = find_price(good_id_list, entp_id_list)
 
     def test_update(self):
         # trans_jsonfile("20220909")
         update_price("20220923")
         update_price("20221007")
         update_price("20221021")
-
-
