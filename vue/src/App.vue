287c461c0bb2ef8a93dfb3af4909602cb7831096
CodeButter96 <90555140+CodeButter96@users.noreply.github.com>
2022-11-14 21:30:52 +0900

    상단 메뉴 추가

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,10 +1,34 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <a href="https://zzangbaguni.shop/map.html">Map</a>
-  </nav>
-  <router-view/>
+  <header>
+    <nav class="navbar navbar-expand-sm navbar-light bg-light">
+      <div class="container-fluid">
+        <router-link to="/" class="navbar-brand">
+          <img alt="Vue logo" src="./assets/logo_small.png" style="height:40px;" />
+          ZzangBaguni
+        </router-link>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarContent">
+          <ul class="navbar-nav me-auto">
+            <li class="nav-item">
+              <router-link to="/" class="nav-link">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/about" class="nav-link">About</router-link>
+            </li>
+            <li class="nav-item">
+              <a href="https://zzangbaguni.shop/map.html" class="nav-link">Map</a>
+            </li>
+          </ul>        
+        </div>
+      </div>   
+    </nav>
+  </header>
+  <main>
+    <router-view/>
+  </main>
+  <footer></footer>
 </template>
 
 <style>
@@ -14,19 +38,16 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;  
-  margin-top: 60px;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
+nav .navbar-nav a.nav-link {
   font-weight: bold;
-  color: #2c3e50;
+  color: #2c3e50 !important;
+  text-align:initial;
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+nav .navbar-nav a.router-link-exact-active {
+  color: #42b983 !important;
 }
+
 </style>
