068d7e12de889abeae6aa8f33fa2edae9af68e4a
박준영 <jun_1204@naver.com>
2022-11-15 21:03:50 +0900

    Cors 적용

--- flaskapp/run.py
+++ flaskapp/run.py
@@ -1,7 +1,8 @@
 from src import create_app
-
+from flask_cors import CORS
 
 app = create_app()
+CORS(app, resources={r'*':{'origins':'http://localhost:8080'}})
 
 if __name__ == '__main__':
     app.run(debug=True)
