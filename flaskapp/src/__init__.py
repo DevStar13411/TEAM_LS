a965e90244f5c2fccdd51f900e10c4c0c006a8ea
Sanghyeop Lee <d0003@naver.com>
2022-11-16 00:38:47 +0900

    create_app에서 flask_cors 적용

--- flaskapp/src/__init__.py
+++ flaskapp/src/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from src.Controller import main_views
+from flask_cors import CORS
+
 
 
 def create_app():
     app = Flask(__name__)
     app.config['JSON_AS_ASCII'] = False
     app.register_blueprint(main_views.main_page)
-
+    CORS(app, resources={r'*':{'origins':'http://localhost:8080'}})
     return app
