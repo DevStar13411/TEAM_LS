1499a03ace88e57c9e919f8826024a9248582560
CodeButter96 <90555140+CodeButter96@users.noreply.github.com>
2022-11-23 01:51:37 +0900

    navigation에서 map 버튼 제거, nginx에서 map.html제공하는 설정 삭제

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -17,9 +17,6 @@
             <li class="nav-item">
               <router-link to="/about" class="nav-link">About</router-link>
             </li>
-            <li class="nav-item">
-              <a href="https://zzangbaguni.shop/map.html" class="nav-link">Map</a>
-            </li>
           </ul>        
         </div>
       </div>   
