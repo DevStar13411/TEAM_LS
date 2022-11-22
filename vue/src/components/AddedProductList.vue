850513042da7b2834cc63ca65abd8645c22e761e
CodeButter96 <90555140+CodeButter96@users.noreply.github.com>
2022-11-23 01:16:01 +0900

    quantity 출력안되는 오류 수정, 지도로 이동 버튼 추가

--- vue/src/components/AddedProductList.vue
+++ vue/src/components/AddedProductList.vue
@@ -4,26 +4,35 @@
       <div>
         <div
           v-if="$store.getters.getcartProducts.length === 0"
-          class="shopped-item"
+          class="shopped-item py-5"
         >
           장바구니에 담은 상품이 없습니다.
         </div>
-        <div
-          v-else
-          class="shopped-item"
-          :key="index"
-          v-for="(cartproduct, index) in $store.getters.getcartProducts"
-        >
-          <div class="shopped-imgtitle">
-            <img class="card-img-top" :src="'https://zzangbaguni.shop/static/img/' + cartproduct.goodId + '.jpg'" alt="https://picsum.photos/300/500">
-            <div class="shopped-title">{{ cartproduct.goodName }}</div>
-          </div>
-          <div id="editquantity">
-            <button @click="addOrder(cartproduct)">+</button>
-            <div>
-              {{ cartproduct.quantity }}
+        <div v-else>
+          <div
+            class="shopped-item"
+            :key="index"
+            v-for="(cartproduct, index) in $store.getters.getcartProducts"
+          >
+            <div class="shopped-imgtitle">
+              <img class="card-img-top" :src="'https://zzangbaguni.shop/static/img/' + cartproduct.goodId + '.jpg'" alt="https://picsum.photos/300/500">
+              <div class="shopped-title">{{ cartproduct.goodName }}</div>
             </div>
-            <button @click="subOrder(cartproduct)">-</button>
+            <div class="row" id="editquantity">
+              <div class="col">
+                <button @click="addOrder(cartproduct)">+</button>
+              </div>
+              <div class="col">
+                {{ cartproduct.goodQuantity }}
+              </div>
+              <div class="col">
+                <button @click="subOrder(cartproduct)">-</button>
+              </div>
+            </div>
+            <hr/>
+          </div>
+          <div class="py-5">
+            <button>지도에서 보기</button>
           </div>
         </div>
       </div>
