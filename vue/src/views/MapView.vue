497bdb60ea453ffef53c2f6b32325722ad0e2816
박준영 <jun_1204@naver.com>
2022-11-27 23:44:16 +0900

    [Front] Map distance 최종

--- vue/src/views/MapView.vue
+++ vue/src/views/MapView.vue
@@ -16,7 +16,7 @@
               <strong>{{place.total_price+"원"}}</strong>
                 </div>
 						</div>
-						<strong>{{place.dist+"km"}}</strong>
+						<strong>{{place.distance/1000+"km"}}</strong>
 
               <div class="d-flex justify-content-end align-items-center">
                 <PriceDetail v-bind:priceInfo="this.price[place.entpId]"></PriceDetail>
@@ -32,7 +32,7 @@
 </template>
 
 <script>
-import haversine from "haversine-distance";
+
 import PriceDetail from "@/components/PriceDetail";
 export default {
 	name: 'MapView',
@@ -203,7 +203,6 @@ export default {
         row["total_price"] = total;
         row["no_product"] = cnt;
       }
-      this.calculate_dist();
       this.sort_entp();
     },
     // 우선 순위 순으로 정렬
@@ -220,21 +219,21 @@ export default {
         } else if (a.total_price < b.total_price) {
           return -1;
         }
-        if (a.dist > b.dist) {
+        if (a.distance > b.distance) {
           return 1;
-        } else if (a.dist < b.dist) {
+        } else if (a.distance < b.distance) {
           return -1;
         }
       });
     },
     // 직선 거리 계산
-    calculate_dist() {
-      for(let i in this.entp){
-        let pos = {latitude: this.entp[i].latitude, longitude: this.entp[i].longitude};
-        let nowPos = {latitude: this.latitude, longitude: this.longitude};
-        this.entp[i].dist = Math.round(haversine(pos, nowPos)) / 1000;
-      }
-    }
+    // calculate_dist() {
+    //   for(let i in this.entp){
+    //     let pos = {latitude: this.entp[i].latitude, longitude: this.entp[i].longitude};
+    //     let nowPos = {latitude: this.latitude, longitude: this.longitude};
+    //     this.entp[i].distance = Math.round(haversine(pos, nowPos)) / 1000;
+    //   }
+    // }
 	},
 	mounted() {
 		let reqBody = {goods: this.$store.getters.getcartOnlyId, latitude: this.latitude, longitude: this.longitude};
