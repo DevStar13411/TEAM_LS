2699ca0d9a39ba9e62d820a12848f4439de027a4
박준영 <jun_1204@naver.com>
2022-11-28 17:25:46 +0900

    [Front] Map 이슈 수정 완료

--- vue/src/views/MapView.vue
+++ vue/src/views/MapView.vue
@@ -23,26 +23,6 @@
 					</div>
 				</div>
 			</div>
-			<div class="list-group list-group-flush border-bottom scrollarea">
-				<div v-for="place in entp" v-bind:key="place.entpId" class="list-group-item list-group-item-action py-3 lh-sm" >
-					<div class="d-flex w-100 align-items-center justify-content-between">
-						  <strong class="mb-1">{{place.entpName}}</strong>
-		  <div>
-						<small class="warning-products" v-if="place.no_product !== 0">⚠️
-			<div class="warning-text">없는 상품이 존재합니다.</div>
-		  </small>
-		  <strong>{{place.total_price}}</strong>
-			</div>
-					</div>
-					<div class="col-10 mb-1 small">Address or Distance</div>
-
-		  <div class="d-flex justify-content-end align-items-center">
-			<PriceDetail v-bind:priceInfo="this.price[place.entpId]"></PriceDetail>
-		  </div>
-		</div>
-			</div>
-			<!-- <div id="pagination"></div> -->
-		</div>
 		<div class="col">
 			<div id="map"></div>			
 		</div>
